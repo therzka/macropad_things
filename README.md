713b1e95bf420f26c9a5cabf926367a6f844ea13
Tali Herzka <taliherzka@gmail.com>
2022-11-26 20:27:25 -0800

    clean up, include zipped lib with dependencies

--- README.md
+++ README.md
@@ -2,6 +2,9 @@ This repo has code I use to personalize my [Adafruit MacroPad RP2040](https://le
 
 
 ## Prerequisite packages to run my code
+
+**Update**: I've included a zipped `lib` directory containing the packages below
+
 You'll need to download the latest [CircuitPython bundle](https://circuitpython.org/libraries) and drag the appropriate packages from the `lib/` directory in the bundle to the `lib/` directory on your mounted macropad (`CIRCUITPY`). [More info here](https://learn.adafruit.com/adafruit-macropad-rp2040/macropad-circuitpython-library)
 
 specifically, you'll need:
@@ -13,6 +16,7 @@ specifically, you'll need:
 - adafruit_hid/ 
 - adafruit_midi/
 - adafruit_ticks.mpy
+
 
 
 ## Adafruit/CircuitPython Resources
--- code.py
+++ code.py
@@ -1,89 +1,94 @@
-# SPDX-FileCopyrightText: 2021 Kattni Rembor for Adafruit Industries
+# SPDX-FileCopyrightText: 2022 Tali Herzka
 #
 # SPDX-License-Identifier: MIT
 
 """Keypad and rotary encoder example for Adafruit MacroPad"""
 
-from adafruit_display_text import bitmap_label as label
-from adafruit_displayio_layout.layouts.grid_layout import GridLayout
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
 from adafruit_macropad import MacroPad
-from rainbowio import colorwheel
-import board
-import digitalio
-import displayio
-import keypad
-import neopixel
-import rotaryio
-import terminalio 
-import time
 import usb_hid
 
 macropad = MacroPad()
-
 pixels = macropad.pixels
-
-
 kbd = Keyboard(usb_hid.devices)
+
 right = Keycode.RIGHT_ARROW
 left = Keycode.LEFT_ARROW
 ctrl = Keycode.LEFT_CONTROL
 cmd = Keycode.COMMAND
 shift = Keycode.LEFT_SHIFT
 opt = Keycode.OPTION
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+YELLOW = (204, 245, 2)
 
 KEY_MAP = {
     # toggle zoom video
-    9: [cmd, shift, Keycode.V],
+    9: ([cmd, shift, Keycode.V], RED),
     # toggle zoom audio
-    10: [cmd, shift, Keycode.A], 
+    10: ([cmd, shift, Keycode.A], RED), 
     # toggle meeting controls
-    11: [ctrl, opt, cmd, Keycode.H]
+    11: ([ctrl, opt, cmd, Keycode.H], YELLOW)
 }
+    
+# set initial key colors
+for k, v in KEY_MAP.items():
+    pixels[k] = v[1]
+    pixels.brightness = 0.3
+    pixels.show()
 
-encoder_last = 0
+# WARNING: this will only be your initial state when you start a meeting if your Zoom settings are set to
+# "mute my mic when joining a meeting" and "stop my video when joining a meeting"
 vol_state = False
 video_state = False
+
+text = macropad.display_text(text_scale=2)
+text[0].text = "MUTED"
+text[1].text = "VIDEO OFF"
+
+# set initial encoder position
+encoder_last = 0
+
+# Toggle LED from green to red or vice-versa
+def toggle_led(key_num):
+    pixels[key_num] = GREEN if pixels[key_num] == RED else RED
 
 while True:
     # Switch desktops via encoder rotation
     encoder_position = macropad.encoder
     if encoder_position > encoder_last:
-        print(encoder_position)
         kbd.press(right, ctrl)
         kbd.release_all()
         encoder_last = encoder_position
     if encoder_position < encoder_last:
-        print(encoder_position)
         kbd.press(left, ctrl)
         kbd.release_all()
         encoder_last = encoder_position
 
-
     # send keyboard events mapped to keypad
     key_event = macropad.keys.events.get()
-    if key_event:
-        print(key_event)
-        
-        if key_event.pressed:
-            key_num = key_event.key_number
-            keys_to_send = KEY_MAP.get(key_num)
-            if keys_to_send:
-                if key_num == 9:
-                    video_state = not video_state
-                if key_num == 10:
-                    title.text = "you are {}muted".format("not " if vol_state else "")
-                    vol_state = not vol_state
-                print("sending keys: {}".format(str(keys_to_send)))
-                kbd.press(*KEY_MAP[key_num])
-                kbd.release_all()
-        else:
-            pass 
+    if key_event and key_event.pressed:
+        key_num = key_event.key_number
+        keys_to_send = KEY_MAP.get(key_num)[0]
+        if keys_to_send:
+            if key_num == 9:
+                video_state = not video_state
+                text[1].text = "VIDEO {}".format("ON" if video_state else "OFF")
+                toggle_led(key_num) 
 
-    # action when rotary switch is pressed
+            if key_num == 10:
+                vol_state = not vol_state
+                text[0].text = "{}MUTED".format("NOT " if vol_state else "")
+                toggle_led(key_num)
+            kbd.press(*KEY_MAP[key_num][0])
+            kbd.release_all()
+
+    # TODO: action when rotary switch is pressed
     macropad.encoder_switch_debounced.update()
     if macropad.encoder_switch_debounced.pressed:
         print("Pressed!")
     if macropad.encoder_switch_debounced.released:
         print("Released!")
+
+    text.show()
