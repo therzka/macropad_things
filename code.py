59019fefa3833424003c344f974006e19bfa1a35
Astatide <astatide@Morgen.local>
2022-12-15 22:02:59 -0500

    Modified keycodes to handle different states

--- code.py
+++ code.py
@@ -17,6 +17,89 @@ rpc = RpcClient()
 hostname = "TEST"
 username = ""
 
+right = Keycode.RIGHT_ARROW
+left = Keycode.LEFT_ARROW
+ctrl = Keycode.LEFT_CONTROL
+cmd = Keycode.COMMAND
+shift = Keycode.LEFT_SHIFT
+opt = Keycode.OPTION
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+YELLOW = (204, 245, 2)
+WHITE = (255, 255, 255)
+
+# maps the numeric macropad key number to a tuple containing an list of key(s) to send
+# and the color to set the key to on the macropad
+TEN_KEY_MAP = {
+    0: ([Keycode.SEVEN], RED),
+    1: ([Keycode.EIGHT], RED),
+    2: ([Keycode.NINE], RED),
+    3: ([Keycode.FOUR], RED),
+    4: ([Keycode.FIVE], RED),
+    5: ([Keycode.SIX], RED),
+    6: ([Keycode.ONE], RED),
+    7: ([Keycode.TWO], RED),
+    8: ([Keycode.THREE], RED),
+    # toggle zoom audio
+    9: ([cmd, shift, Keycode.A], RED),
+
+    # toggle zoom video
+    10: ([cmd, shift, Keycode.V], RED), 
+    
+    # toggle zoom floating meeting controls
+    11: ([ctrl, opt, cmd, Keycode.H], YELLOW)
+}
+TEST_KEY_MAP = {
+    0: ([Keycode.ONE], WHITE),
+    1: ([Keycode.TWO], WHITE),
+    2: ([Keycode.THREE], WHITE),
+    3: ([Keycode.FOUR], WHITE),
+    4: ([Keycode.FIVE], WHITE),
+    5: ([Keycode.SIX], WHITE),
+    6: ([Keycode.SEVEN], WHITE),
+    7: ([Keycode.EIGHT], WHITE),
+    8: ([Keycode.NINE], WHITE),
+    # toggle zoom audio
+    9: ([cmd, shift, Keycode.A], RED),
+
+    # toggle zoom video
+    10: ([cmd, shift, Keycode.V], RED), 
+    
+    # toggle zoom floating meeting controls
+    11: ([ctrl, opt, cmd, Keycode.H], YELLOW)
+}
+
+TEN_KEY = {
+    "KEYMAP": TEN_KEY_MAP,
+    "NAME": "10-Key"
+    }
+TEST = {
+    "KEYMAP": TEST_KEY_MAP,
+    "NAME": "Test"
+    }
+HA = {
+    "KEYMAP": TEN_KEY_MAP,
+    "NAME": "Home Assistant"
+    }
+
+GLOBAL_STATES = { 0: TEN_KEY, 1: TEST, 2: HA }
+pixels = macropad.pixels
+text = macropad.display_text(text_scale=1)
+
+def center_text(text, char=21):
+    return f"{text : ^{char}}"
+def right_text(text, char=21):
+    return f"{text : >21}"
+def left_text(text, char=21):
+    return f"{text : <21}"
+
+def set_global_state(state=0):
+    pixels.brightness = 0.2
+    for k, v in GLOBAL_STATES[state]["KEYMAP"].items():
+        pixels[k] = v[1]
+        pixels.show()
+    text[2].text = right_text(GLOBAL_STATES[state]["NAME"])
+
 async def rpc_call(function, *args, **kwargs):
     responseTask = asyncio.create_task(rpc.call(function, *args, **kwargs))
     response = (await asyncio.gather(responseTask))[0]
@@ -62,48 +145,11 @@ async def main():
     username = "A"
     host_user_task = asyncio.create_task(connect_and_get_username())
     hostUserNameFuture = asyncio.gather(host_user_task)
-    pixels = macropad.pixels
 
     kbd = Keyboard(usb_hid.devices)
-
-    right = Keycode.RIGHT_ARROW
-    left = Keycode.LEFT_ARROW
-    ctrl = Keycode.LEFT_CONTROL
-    cmd = Keycode.COMMAND
-    shift = Keycode.LEFT_SHIFT
-    opt = Keycode.OPTION
-    RED = (255, 0, 0)
-    GREEN = (0, 255, 0)
-    YELLOW = (204, 245, 2)
-    WHITE = (255, 255, 255)
-
-    # maps the numeric macropad key number to a tuple containing an list of key(s) to send
-    # and the color to set the key to on the macropad
-    KEY_MAP = {
-        0: ([Keycode.SEVEN], WHITE),
-        1: ([Keycode.EIGHT], WHITE),
-        2: ([Keycode.NINE], WHITE),
-        3: ([Keycode.FOUR], WHITE),
-        4: ([Keycode.FIVE], WHITE),
-        5: ([Keycode.SIX], WHITE),
-        6: ([Keycode.ONE], WHITE),
-        7: ([Keycode.TWO], WHITE),
-        8: ([Keycode.THREE], WHITE),
-        # toggle zoom audio
-        9: ([cmd, shift, Keycode.A], RED),
-
-        # toggle zoom video
-        10: ([cmd, shift, Keycode.V], RED), 
-        
-        # toggle zoom floating meeting controls
-        11: ([ctrl, opt, cmd, Keycode.H], YELLOW)
-    }
         
     # set initial key colors and brightness
-    pixels.brightness = 0.2
-    for k, v in KEY_MAP.items():
-        pixels[k] = v[1]
-        pixels.show()
+    set_global_state()
 
     # WARNING: this will only be your initial state when you start a meeting if your Zoom settings are set to
     # "mute my mic when joining a meeting" and "stop my video when joining a meeting" and will only accurately
@@ -114,14 +160,6 @@ async def main():
     ## set initial display text
     ## At a scale of 1m we just hit the 1 on the 13... so we have about 22 characters.
 
-    def center_text(text, char=21):
-        return f"{text : ^{char}}"
-    def right_text(text, char=21):
-        return f"{text : >21}"
-    def left_text(text, char=21):
-        return f"{text : <21}"
-
-    text = macropad.display_text(text_scale=1)
     text[0].text = center_text(f"{username}@{hostname}")
     text[1].text = right_text("BETA")
 
@@ -144,7 +182,9 @@ async def main():
         else:
             # macropad.play_file(ON_AUDIO)
             pixels[key_num] = GREEN
+
     hostnameSet = False
+    ACTIVE_STATE = 0
     while True:
         if (not hostnameSet and host_user_task.done()):
             hostname, username = (await hostUserNameFuture)[0]
@@ -167,7 +207,7 @@ async def main():
         if key_event:
             if key_event.pressed:
                 key_num = key_event.key_number
-                keys_to_send = KEY_MAP.get(key_num)[0]
+                keys_to_send = GLOBAL_STATES[ACTIVE_STATE]["KEYMAP"].get(key_num)[0]
                 if keys_to_send:
                     # toggle zoom video
                     if key_num == 9:
@@ -181,7 +221,7 @@ async def main():
                     
                     toggle_led_and_sound(key_num)
 
-                    keys_to_press = KEY_MAP[key_num][0]
+                    keys_to_press = GLOBAL_STATES[ACTIVE_STATE]["KEYMAP"][key_num][0]
                     if keys_to_press:
                         kbd.press(*keys_to_press)
                     kbd.release_all()
@@ -189,9 +229,15 @@ async def main():
         # TODO: action when rotary switch is pressed
         macropad.encoder_switch_debounced.update()
         if macropad.encoder_switch_debounced.pressed:
-            text[2].text = "Pressed!"
-        if macropad.encoder_switch_debounced.released:
-            text[2].text = "Released!"
+            print(ACTIVE_STATE)
+            if (ACTIVE_STATE + 1 == len(GLOBAL_STATES)):
+                ACTIVE_STATE = 0
+                set_global_state(0)
+            else:
+                ACTIVE_STATE += 1
+                set_global_state(ACTIVE_STATE)
+        # if macropad.encoder_switch_debounced.released:
+        #     text[2].text = "Released!"
 
         text.show()
         await asyncio.sleep(0.01)
